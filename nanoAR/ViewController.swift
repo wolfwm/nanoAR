967d8ea4c28d6a819008eda58c32db06396bc563
Wolfgang Walder <wolfwalder@gmail.com>
2020-02-14 16:11:50 -0300

    initial commit

---
+++ nanoAR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nanoAR
+//
+//  Created by Wolfgang Walder on 14/02/20.
+//  Copyright © 2020 Wolfgang Walder. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
