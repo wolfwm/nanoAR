d3531f11fea063a0949bf40d58b935827d3ff185
Mariana Lima <mrl.lima85@gmail.com>
2020-02-19 15:07:28 -0300

    adicionado botoes, posicionamento do botoes e mudanca do tamanho do frame

--- nanoAR/Controller/ViewController.swift
+++ nanoAR/Controller/ViewController.swift
@@ -24,6 +24,24 @@ class ViewController: UIViewController, ARSessionDelegate {
     let squareOffset: SIMD3<Float> = [0,0,0]
     var squareAnchor = AnchorEntity()
     
+    let buttonScale: SIMD3<Float> = [0.2, 0.2, 0.2]
+    
+    var trButton: ModelEntity?
+    let trbOffset: SIMD3<Float> = [0.8,0.6,0]
+    var trbAnchor = AnchorEntity()
+    
+    var tlButton: ModelEntity?
+    let tlbOffset: SIMD3<Float> = [-0.8,0.6,0]
+    var tlbAnchor = AnchorEntity()
+    
+    var brButton: ModelEntity?
+    let brbOffset: SIMD3<Float> = [0.8,-0.6,0]
+    var brbAnchor = AnchorEntity()
+
+    var blButton: ModelEntity?
+    let blbOffset: SIMD3<Float> = [-0.8,-0.6,0]
+    var blbAnchor = AnchorEntity()
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         arView.session.delegate = self
@@ -37,10 +55,14 @@ class ViewController: UIViewController, ARSessionDelegate {
         // Run a body tracking configration.
         let configuration = ARBodyTrackingConfiguration()
         arView.session.run(configuration)
-        
+
         arView.scene.addAnchor(characterAnchor)
         arView.scene.addAnchor(squareAnchor)
-        
+        arView.scene.addAnchor(trbAnchor)
+        arView.scene.addAnchor(tlbAnchor)
+        arView.scene.addAnchor(brbAnchor)
+        arView.scene.addAnchor(blbAnchor)
+
         // Asynchronously load the 3D character.
         var cancellable: AnyCancellable? = nil
         cancellable = Entity.loadBodyTrackedAsync(named: "character/robot").sink(
@@ -70,15 +92,90 @@ class ViewController: UIViewController, ARSessionDelegate {
         }, receiveValue: { (square: Entity) in
             if let square = square as? ModelEntity {
                 // Scale the character to human size
-                square.scale = [1, 1, 1]
+                square.scale = [1.1, 1.3, 1.3]
                 self.square = square
                 cancellableSquare?.cancel()
             } else {
-                print("Error: Unable to load model as BodyTrackedEntity")
+                print("Error: Unable to load model as Entity")
             }
             
         })
         
+        var cancellableTRButton: AnyCancellable? = nil
+        cancellableTRButton = Entity.loadModelAsync(named: "3dobjects/button.obj").sink(
+            receiveCompletion: { completion in
+                if case let .failure(error) = completion {
+                    print("Error: Unable to load model: \(error.localizedDescription)")
+                }
+                cancellableTRButton?.cancel()
+        }, receiveValue: { (button: Entity) in
+            if let button = button as? ModelEntity {
+                // Scale the character to human size
+                button.scale = self.buttonScale
+                self.trButton = button
+                cancellableTRButton?.cancel()
+            } else {
+                print("Error: Unable to load model as Entity")
+            }
+            
+        })
+        
+        var cancellableTLButton: AnyCancellable? = nil
+        cancellableTLButton = Entity.loadModelAsync(named: "3dobjects/button.obj").sink(
+            receiveCompletion: { completion in
+                if case let .failure(error) = completion {
+                    print("Error: Unable to load model: \(error.localizedDescription)")
+                }
+                cancellableTLButton?.cancel()
+        }, receiveValue: { (button: Entity) in
+            if let button = button as? ModelEntity {
+                // Scale the character to human size
+                button.scale = self.buttonScale
+                self.tlButton = button
+                cancellableTLButton?.cancel()
+            } else {
+                print("Error: Unable to load model as Entity")
+            }
+            
+        })
+        
+        var cancellableBRButton: AnyCancellable? = nil
+        cancellableBRButton = Entity.loadModelAsync(named: "3dobjects/button.obj").sink(
+            receiveCompletion: { completion in
+                if case let .failure(error) = completion {
+                    print("Error: Unable to load model: \(error.localizedDescription)")
+                }
+                cancellableBRButton?.cancel()
+        }, receiveValue: { (button: Entity) in
+            if let button = button as? ModelEntity {
+                // Scale the character to human size
+                button.scale = self.buttonScale
+                self.brButton = button
+                cancellableBRButton?.cancel()
+            } else {
+                print("Error: Unable to load model as Entity")
+            }
+            
+        })
+        
+        var cancellableBLButton: AnyCancellable? = nil
+        cancellableBLButton = Entity.loadModelAsync(named: "3dobjects/button.obj").sink(
+            receiveCompletion: { completion in
+                if case let .failure(error) = completion {
+                    print("Error: Unable to load model: \(error.localizedDescription)")
+                }
+                cancellableBLButton?.cancel()
+        }, receiveValue: { (button: Entity) in
+            if let button = button as? ModelEntity {
+                // Scale the character to human size
+                button.scale = self.buttonScale
+                self.blButton = button
+                cancellableBLButton?.cancel()
+            } else {
+                print("Error: Unable to load model as Entity")
+            }
+            
+        })
     }
     
     func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
@@ -90,18 +187,30 @@ class ViewController: UIViewController, ARSessionDelegate {
             let bodyPosition = simd_make_float3(bodyAnchor.transform.columns.3)
             
             characterAnchor.position = bodyPosition + characterOffset
-            squareAnchor.position = bodyPosition
+            squareAnchor.position = bodyPosition + squareOffset
+            trbAnchor.position = bodyPosition + trbOffset
+            tlbAnchor.position = bodyPosition + tlbOffset
+            brbAnchor.position = bodyPosition + brbOffset
+            blbAnchor.position = bodyPosition + blbOffset
             // Also copy over the rotation of the body anchor, because the skeleton's pose
             // in the world is relative to the body anchor's rotation.
             characterAnchor.orientation = Transform(matrix: bodyAnchor.transform).rotation
-            squareAnchor.orientation = Transform(matrix: bodyAnchor.transform).rotation
             
-            if let character = character, character.parent == nil, let square = square, square.parent == nil {
+            if let character = character, character.parent == nil, let square = square, square.parent == nil, let trButton = trButton, trButton.parent == nil, let tlButton = tlButton, tlButton.parent == nil, let brButton = brButton, brButton.parent == nil, let blButton = blButton, blButton.parent == nil {
                 // Attach the character to its anchor as soon as
                 // 1. the body anchor was detected and
                 // 2. the character was loaded.
                 characterAnchor.addChild(character)
                 squareAnchor.addChild(square)
+                trbAnchor.addChild(trButton)
+                tlbAnchor.addChild(tlButton)
+                brbAnchor.addChild(brButton)
+                blbAnchor.addChild(blButton)
+                
+                trButton.orientation = simd_quatf(angle: .pi/2, axis: [0,0,1])
+                brButton.orientation = simd_quatf(angle: .pi/2, axis: [0,0,1])
+                tlButton.orientation = simd_quatf(angle: -(.pi/2), axis: [0,0,1])
+                blButton.orientation = simd_quatf(angle: -(.pi/2), axis: [0,0,1])
             }
         }
     }
